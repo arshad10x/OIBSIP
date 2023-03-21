3c990d9dec729a8d28530bdb17645411754cc816
Arshad Ahmed <arshadahmed10x@gmail.com>
2023-03-21 16:23:03 +0530

    initial commit

--- task 3/script.js
+++ task 3/script.js
@@ -1,47 +1,98 @@
-(function(){
-  
-    var list = document.querySelector('#list'),
-        form = document.querySelector('form'),
-        item = document.querySelector('#item');
-    
-        function updateCurrentDate() {
-            const now = new Date();
-            const currentDateElement = document.getElementById("current-date");
-            currentDateElement.innerHTML = now.toLocaleString();
-          }
-      
-          // Update the date and time every second (1000 milliseconds)
-          setInterval(updateCurrentDate, 1000);
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the input field, button, and list
+  const input = document.getElementById('item');
+  const addButton = document.getElementById('add-button');
+  const list = document.getElementById('list');
 
-    form.addEventListener('submit',function(e){
-      e.preventDefault();
-      list.innerHTML += '<li>' + item.value + '</li>';
-      store();
-      item.value = "";
-    },false)
-    
-    list.addEventListener('click',function(e){
-      var t = e.target;
-      if(t.classList.contains('checked')){
-        t.parentNode.removeChild(t);
-      } else {
-        t.classList.add('checked');
-      }
-      store();
-    },false)
-    
-    function store() {
-      window.localStorage.myitems = list.innerHTML;
+  // Create an empty array to store the tasks
+  let tasks = [];
+
+  // Add event listener for Add button
+  addButton.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // Get the input value and trim whitespace
+    const inputValue = input.value.trim();
+
+    // If input is not empty, add task to array and update UI
+    if (inputValue !== '') {
+      tasks.push(inputValue);
+      input.value = '';
+      updateUI();
     }
-    
-    function getValues() {
-      var storedValues = window.localStorage.myitems;
-      if(!storedValues) {
-        list.innerHTML = '<li>Make a to do list</li>';
-      }
-      else {
-        list.innerHTML = storedValues;
-      }
+  });
+
+  // Update UI to display tasks
+  function updateUI() {
+    // Clear existing list
+    list.innerHTML = '';
+
+    // Create a new list item for each task
+    for (let i = 0; i < tasks.length; i++) {
+      const task = tasks[i];
+
+      // Create new list item and text node
+      const listItem = document.createElement('li');
+      const textNode = document.createElement('span');
+      textNode.textContent = task;
+
+      // Create delete button and add event listener
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.addEventListener('click', function() {
+        deleteTask(i);
+      });
+
+      // Create edit button and add event listener
+      const editButton = document.createElement('button');
+      editButton.textContent = 'Edit';
+      editButton.addEventListener('click', function() {
+        editTask(i, listItem, textNode);
+      });
+
+      // Append text node and buttons to list item
+      listItem.appendChild(textNode);
+      listItem.appendChild(deleteButton);
+      listItem.appendChild(editButton);
+
+      // Append list item to list
+      list.appendChild(listItem);
     }
-    getValues();
-  })();
+  }
+
+  // Delete task from array and update UI
+  function deleteTask(index) {
+    tasks.splice(index, 1);
+    updateUI();
+  }
+
+  // Edit task in array and update UI
+  function editTask(index, listItem, textNode) {
+    // Replace text node with input field
+    const inputField = document.createElement('input');
+    inputField.type = 'text';
+    inputField.value = textNode.textContent;
+    listItem.replaceChild(inputField, textNode);
+
+    // Replace edit button with save button
+    const saveButton = document.createElement('button');
+    saveButton.textContent = 'Save';
+    listItem.replaceChild(saveButton, listItem.childNodes[1]);
+
+    // Update task in array and text node in UI on Save button click
+    saveButton.addEventListener('click', function() {
+      const newTask = inputField.value.trim();
+
+      // If input is not empty, update task in array and text node in UI
+      if (newTask !== '') {
+        tasks[index] = newTask;
+        listItem.replaceChild(textNode, inputField);
+        saveButton.textContent = 'Edit';
+        updateUI();
+      }
+    });
+  }
+
+  // Call updateUI() on page load
+  updateUI();
+});
